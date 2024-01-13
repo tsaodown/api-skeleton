572d90c043e92e013be5c63bc13d7be231762e8a
Edd Tsao <etphonehomies@gmail.com>
2024-01-12 16:59:27 -0800

    Add more tests

--- tests/test_appts.py
+++ tests/test_appts.py
@@ -13,6 +13,20 @@ def data_appointment_doc_1_appt_1(db):
 
 
 @pytest.fixture
+def data_appointment_doc_1_appt_1_2(db):
+    db.session.add(Appointment(doctor_id=1, start_time=datetime.fromisoformat(
+        '2021-01-01T10:15:00'), end_time=datetime.fromisoformat('2021-01-01T10:30:00')))
+    db.session.commit()
+
+
+@pytest.fixture
+def data_appointment_doc_1_appt_1_3(db):
+    db.session.add(Appointment(doctor_id=1, start_time=datetime.fromisoformat(
+        '2021-01-01T10:45:00'), end_time=datetime.fromisoformat('2021-01-01T11:00:00')))
+    db.session.commit()
+
+
+@pytest.fixture
 def data_appointment_doc_2_appt_1(db):
     db.session.add(Appointment(doctor_id=2, start_time=datetime.fromisoformat(
         '2021-01-01T09:00:00'), end_time=datetime.fromisoformat('2021-01-01T10:00:00')))
@@ -44,6 +58,20 @@ def test_create_appointment_happy_path(client):
     assert res.json.get('data').get('doctor').get('id') == 1
     assert res.json.get('data').get('start_time') == '2021-01-01T09:00:00'
     assert res.json.get('data').get('end_time') == '2021-01-01T10:00:00'
+
+
+@pytest.mark.usefixtures('data_appointment_doc_1_appt_1', 'data_appointment_doc_1_appt_1_2', 'data_appointment_doc_1_appt_1_3')
+def test_create_appointment_happy_path_between(client):
+    res = client.post('/appts/create', query_string={
+        'doctor_id': 1,
+        'start': '2021-01-01T10:05:00',
+        'end': '2021-01-01T10:10:00'
+    })
+
+    assert res.status_code == HTTPStatus.OK
+    assert res.json.get('data').get('doctor').get('id') == 1
+    assert res.json.get('data').get('start_time') == '2021-01-01T10:05:00'
+    assert res.json.get('data').get('end_time') == '2021-01-01T10:10:00'
 
 
 def test_create_appointment_missing_doctor(client):
@@ -84,6 +112,42 @@ def test_create_appointment_doctor_busy(client):
         'doctor_id': 1,
         'start': '2021-01-01T09:30:00',
         'end': '2021-01-01T10:30:00'
+    })
+
+    assert res.status_code == HTTPStatus.NOT_FOUND
+    assert res.json.get('error') == 'Doctor not available'
+
+
+@pytest.mark.usefixtures('data_appointment_doc_1_appt_1', 'data_appointment_doc_1_appt_1_2', 'data_appointment_doc_1_appt_1_3')
+def test_create_appointment_partial_overlap_1(client):
+    res = client.post('/appts/create', query_string={
+        'doctor_id': 1,
+        'start': '2021-01-01T10:10:00',
+        'end': '2021-01-01T10:30:00'
+    })
+
+    assert res.status_code == HTTPStatus.NOT_FOUND
+    assert res.json.get('error') == 'Doctor not available'
+
+
+@pytest.mark.usefixtures('data_appointment_doc_1_appt_1', 'data_appointment_doc_1_appt_1_2', 'data_appointment_doc_1_appt_1_3')
+def test_create_appointment_partial_overlap_2(client):
+    res = client.post('/appts/create', query_string={
+        'doctor_id': 1,
+        'start': '2021-01-01T10:25:00',
+        'end': '2021-01-01T10:40:00'
+    })
+
+    assert res.status_code == HTTPStatus.NOT_FOUND
+    assert res.json.get('error') == 'Doctor not available'
+
+
+@pytest.mark.usefixtures('data_appointment_doc_1_appt_1', 'data_appointment_doc_1_appt_1_2', 'data_appointment_doc_1_appt_1_3')
+def test_create_appointment_partial_overlap_3(client):
+    res = client.post('/appts/create', query_string={
+        'doctor_id': 1,
+        'start': '2021-01-01T10:50:00',
+        'end': '2021-01-01T11:10:00'
     })
 
     assert res.status_code == HTTPStatus.NOT_FOUND
@@ -141,6 +205,39 @@ def test_get_appointments_happy_path_2(client):
     assert res.json.get('data')[0].get('doctor').get('id') == 2
     assert res.json.get('data')[0].get('start_time') == '2021-01-01T09:00:00'
     assert res.json.get('data')[0].get('end_time') == '2021-01-01T10:00:00'
+
+
+@pytest.mark.usefixtures('data_appointment_doc_1_appt_1', 'data_appointment_doc_1_appt_1_2', 'data_appointment_doc_1_appt_1_3')
+def test_get_appointments_partial_overlap_1(client):
+    res = client.get('/appts/', query_string={
+        'doctor_id': 1,
+        'start': '2021-01-01T09:45:00',
+        'end': '2021-01-01T10:10:00'
+    })
+
+    assert res.status_code == HTTPStatus.OK
+    assert len(res.json.get('data')) == 1
+    assert res.json.get('data')[0].get('doctor').get('id') == 1
+    assert res.json.get('data')[0].get('start_time') == '2021-01-01T09:00:00'
+    assert res.json.get('data')[0].get('end_time') == '2021-01-01T10:00:00'
+
+
+@pytest.mark.usefixtures('data_appointment_doc_1_appt_1', 'data_appointment_doc_1_appt_1_2', 'data_appointment_doc_1_appt_1_3')
+def test_get_appointments_partial_overlap_2(client):
+    res = client.get('/appts/', query_string={
+        'doctor_id': 1,
+        'start': '2021-01-01T09:45:00',
+        'end': '2021-01-01T10:20:00'
+    })
+
+    assert res.status_code == HTTPStatus.OK
+    assert len(res.json.get('data')) == 2
+    assert res.json.get('data')[0].get('doctor').get('id') == 1
+    assert res.json.get('data')[0].get('start_time') == '2021-01-01T09:00:00'
+    assert res.json.get('data')[0].get('end_time') == '2021-01-01T10:00:00'
+    assert res.json.get('data')[1].get('doctor').get('id') == 1
+    assert res.json.get('data')[1].get('start_time') == '2021-01-01T10:15:00'
+    assert res.json.get('data')[1].get('end_time') == '2021-01-01T10:30:00'
 
 
 def test_get_appointments_missing_doctor(client):
